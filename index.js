09db9af3a030f5aa9b870e6903c4b59a398bcc50
jamesmcdonald112 <jamesmcdonald112@yahoo.ie>
2024-06-21 14:25:16 +0100

    Finished stage 1/3

---
+++ index.js
@@ -0,0 +1,27 @@
+// Welcome to the Chalkboard Printer!
+const input = require('sync-input');
+
+// These variables are already declared for you.
+// Looks like we made a mistake and used 'var' instead of 'let'!
+// Remember it's bad practice to use 'var', so let's change all of them to 'let'.
+let name, surname, message;
+name = "Bart";
+surname = "Simpson";
+message = "I will not skateboard in the halls.";
+
+// Assign the correct values to the variables.
+
+// Log the message to the console 5 times.
+let count = 0;
+while(count < 5) {
+    console.log(`This is ${name} ${surname} and ${message}`);
+    count++;
+}
+// etc...
+
+// We have also imported the 'sync-input' package for you.
+// You will use this in later stages.
+// This package allows you to get user input.
+// Like so:
+// let name = input("Type your name: ");
+// let age = Number(input("Type your age: "));
