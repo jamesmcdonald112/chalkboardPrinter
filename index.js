10ebba3793dca0501a4415d044bc11c89fe1b480
jamesmcdonald112 <jamesmcdonald112@yahoo.ie>
2024-06-21 15:35:26 +0100

    Finished project. User can now select the number of times the message repeats

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 // Welcome to the Chalkboard Printer!
 const input = require('sync-input');
 
-// These variables are already declared for you.
-// Looks like we made a mistake and used 'var' instead of 'let'!
-// Remember it's bad practice to use 'var', so let's change all of them to 'let'.
-let name, surname, message;
-name = "Bart";
-surname = "Simpson";
-message = "I will not skateboard in the halls.";
 
-// Assign the correct values to the variables.
+let name, surname, message, repeats;
+name = input("Enter name: ");
+surname = input("Enter surname: ");
+message = input("Enter message: ");
+repeats = input("Enter number of repeats: ")
 
-// Log the message to the console 5 times.
+
 let count = 0;
-while(count < 5) {
+while(count < repeats) {
     console.log(`This is ${name} ${surname} and ${message}`);
     count++;
 }
-// etc...
-
-// We have also imported the 'sync-input' package for you.
-// You will use this in later stages.
-// This package allows you to get user input.
-// Like so:
-// let name = input("Type your name: ");
-// let age = Number(input("Type your age: "));
